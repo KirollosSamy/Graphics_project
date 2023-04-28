b28e3462572fb01dd313e86005b59ee036c6e963
shredan <shredanabdullah@gmail.com>
2023-04-28 17:25:05 +0300

    req.4,7

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -3,11 +3,18 @@
 #include "../asset-loader.hpp"
 #include "deserialize-utils.hpp"
 
+/*Materialclass represents a basic material that can be used to render objects in the game engine.
+It contains a PipelineState object that holds information about the rendering pipeline 
+and a ShaderProgram object that holds the shader code for rendering. 
+The setup() function sets up the pipeline state and uses the shader program.*/
+
 namespace our {
 
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
         //TODO: (Req 7) Write this function
+        pipelineState.setup();
+        shader->use();
     }
 
     // This function read the material data from a json object
@@ -23,8 +30,15 @@ namespace our {
 
     // This function should call the setup of its parent and
     // set the "tint" uniform to the value in the member variable tint 
+    
+    /*TintedMaterial class extends Material and
+    adds a glm::vec4 variable tint that represents the color tint to be applied to the object.
+    The setup() function first calls the Material::setup() function to set up the pipeline state and use the shader program,
+    then it sets the tint uniform in the shader program.*/
     void TintedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+        Material::setup();
+        shader->set("tint", tint);
     }
 
     // This function read the material data from a json object
@@ -37,8 +51,17 @@ namespace our {
     // This function should call the setup of its parent and
     // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
+
+    /*TexturedMaterial class extends TintedMaterial and adds a Texture2D object and a Sampler object for texturing the object.
+     The setup() function first calls the TintedMaterial::setup() function to set up the tint and pipeline state,
+     then it sets the alphaThreshold uniform in the shader program and binds the texture and sampler to a texture unit.*/
     void TexturedMaterial::setup() const {
         //TODO: (Req 7) Write this function
+         TintedMaterial::setup();
+        shader->set("alphaThreshold", alphaThreshold);
+        texture->bind();
+        sampler->bind(0);
+        shader->set("tex", 0);
     }
 
     // This function read the material data from a json object
@@ -51,3 +74,10 @@ namespace our {
     }
 
 }
+ /*
+deserialize() function that reads the material data from a JSON object.
+These functions check for the existence of certain keys in the JSON object,
+such as pipelineState, shader, transparent, tint, alphaThreshold, texture, and sampler, and set the corresponding member variables accordingly.
+The setup() functions in all three classes are part of the rendering pipeline and
+are called every frame before rendering an object with that material.
+They set up the pipeline state and use the shader program to render the object.*/
--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -37,12 +37,72 @@ namespace our {
         glm::bvec4 colorMask = {true, true, true, true}; // To know how to use it, check glColorMask
         bool depthMask = true; // To know how to use it, check glDepthMask
 
+        //i check it but didn't get the idea behind(the how to use)
+
 
         // This function should set the OpenGL options to the values specified by this structure
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 4) Write this function
-        }
+            // Enable or disable face culling based on the value of faceCulling.enabled
+
+            //face culling refers to the process of determining which faces
+            //of a three-dimensional object are visible or hidden from a particular viewpoint.
+            if (faceCulling.enabled) {
+                glEnable(GL_CULL_FACE);
+                //Once enabled, you can specify which faces should
+                //be culled and the order in which they should be rendered using
+                //the glCullFace() and glFrontFace() functions, respectively.
+                glCullFace(faceCulling.culledFace);//specifies which faces of a three-dimensional object should be culled
+                glFrontFace(faceCulling.frontFace);//specifies the orientation of the front face of the object
+            } else {
+                glDisable(GL_CULL_FACE);
+            }
+            
+            // Set the depth test based on the value of depthTest.enabled
+            if (depthTesting.enabled) {
+                glEnable(GL_DEPTH_TEST);//When depth testing is enabled, OpenGL determines which fragments of
+                // a three-dimensional object should be drawn based on their distance from the viewer. This helps to ensure that objects appear
+                // to be drawn in the correct order and that objects that are farther away from the viewer are obscured by objects that are closer.
+                glDepthFunc(depthTesting.function);
+                //to set the depth testing function for subsequent rendering operations.
+                glDepthMask(depthMask);
+                //hia hena m3molha set b true dayman i think
+                // If depthMask is set to GL_TRUE, the depth buffer is writable and fragments that pass the depth test will update the depth buffer.
+                // If depthMask is set to GL_FALSE, the depth buffer is read-only and fragments will not update the depth buffer.
+            } else {
+                glDisable(GL_DEPTH_TEST);
+            }
+            
+            // Enable or disable blending based on the value of blend.enabled
+            if (blending.enabled) {
+                glEnable(GL_BLEND);
+                //When blending is enabled,fragments that are drawn to the framebuffer
+                //are combined with the existing color values in the framebuffer according to a blending equation.
+                glBlendEquation(blending.equation);// the equation used is GL_FUNC_ADD means that the source and destination colors are added together.
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+                // sets the blending factors for source and destination colors.
+                // The source color is the color of the pixel currently being drawn, (GL_SRC_ALPHA)the alpha value of the source color.
+                // while the destination color is the color of the pixel already in the framebuffer.
+                glBlendColor(blending.constantColor[0],blending.constantColor[1],blending.constantColor[2],blending.constantColor[3]);
+                //sets a constant blend color to be used in blending operations.
+                // It takes four parameters representing the red, green, blue, and alpha components of the color.
+                
+            } else {
+                glDisable(GL_BLEND);
+            }
+            glColorMask(colorMask[0], colorMask[1], colorMask[2], colorMask[3]);
+            ///specifies whether color components should be written to the color buffer during rendering.
+            // It takes four boolean parameters that correspond to the red, green, blue, and alpha channels of the color buffer, respectively.
+            //colorMask is a four-element boolean bvec4 (glm::bvec4 colorMask = {true, true, true, true}; )
+            //if colorMask is set to [true, true, true, true], all color components will be written to the color buffer.
+            // If colorMask is set to [true, false, true, false],
+            // only the red and blue color components will be written to the color buffer,
+            // while the green and alpha components will be masked out (not written to the buffer).
+            //this can be useful in certain rendering scenarios where we want to selectively render certain color channels or control the blending of multiple render passes.
+            }
+            
+          
 
         // Given a json object, this function deserializes a PipelineState structure
         void deserialize(const nlohmann::json& data);
