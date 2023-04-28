eecfe3835d97a8bc23e3fc42ac10a4649ffe5593
nadaaosman <84284180+nadaaosman@users.noreply.github.com>
2023-04-28 17:31:15 +0300

    all comments are DONE 2,6,9,10

--- source/common/mesh/mesh.hpp
+++ source/common/mesh/mesh.hpp
@@ -30,50 +30,75 @@ namespace our {
         {
     elementCount= elements.size();
 
+    //TODO: (Req 2) Write this function
+    // remember to store the number of elements in "elementCount" since you will need it for drawing
+    // For the attribute locations, use the constants defined above: ATTRIB_LOC_POSITION, ATTRIB_LOC_COLOR, etc     
+    
+    
+    // the element buffer (EBO) identify the order of the vertices but the data is in the vertex buffer (VBO)
+    
+    //The glBindBuffer function binds the buffer object with the specified name to the specified buffer target.
+    //In this case, it binds the VBO to the GL_ARRAY_BUFFER target and the EBO to the GL_ELEMENT_ARRAY_BUFFER target.
+    //to make it know that iam talking about this VBO and EBO
+
+    //The glBufferData function creates and initializes a buffer object's data store.
+    //It takes the buffer target, the size of the data in bytes, a pointer to the data.
+
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
 
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size()*sizeof(uint16_t), elements.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size()*sizeof(unsigned int), elements.data(), GL_STATIC_DRAW);
+
 
     //bind the vertex array
+    //to bind the vertex array with the vertex buffer
+    // we want to declare that this vertex array which will read from the buffers and send to the vertex shader
+    //The glBindVertexArray function binds the specified vertex array object.
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
- 
+
+    //this the position which the vertex shader will read from
+    //glVertexAttribPointer--> this to make it understand how to read the bits
+    //i gave it the position and 3 as they are three dimensions and the size to know how to jump to the another element
+    //this to change the data from c++ not from the vertex shader , i can change the position from here.
+    // The glEnableVertexAttribArray function enables the generic vertex attribute array specified by position_loc, color_loc, texture_loc, and normal_loc.
     GLint position_loc = ATTRIB_LOC_POSITION;
     glEnableVertexAttribArray(position_loc);
     glVertexAttribPointer(position_loc, 3, GL_FLOAT, false, sizeof(Vertex), 0);
 
+    //4 as red blue green alpha not 3 as the positions x y z
+    //the last parameter --> to know where to start the color , it is not from zero like the position but it is the offsetof (vertex ,color) , how many step to move in the vertex struct to get the color
+    //GL_UNSIGNED_BYTE , the type of the color
+    //normalized (true) to get the max 255 and divide all by it  before sending to the vertex shader
     GLint color_loc = ATTRIB_LOC_COLOR;
     glEnableVertexAttribArray(color_loc);
     glVertexAttribPointer(color_loc, 4, GL_UNSIGNED_BYTE, true, sizeof(Vertex), (void*)offsetof(Vertex, color));
 
+    // we set up the texture coordinate vertex attribute for the shader program.
+    // 2---> for texture coordinates (x,y)-->2D texture
     GLint texture_loc = ATTRIB_LOC_TEXCOORD;
     glEnableVertexAttribArray(texture_loc); 
     glVertexAttribPointer(texture_loc, 2, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex, tex_coord));
 
+    // sets up the normal vector vertex attribute for the shader program.
+    // 3---> for normal vector components (X,y,z) 
     GLint normal_loc = ATTRIB_LOC_NORMAL;
     glEnableVertexAttribArray(normal_loc); 
     glVertexAttribPointer(normal_loc, 3, GL_FLOAT, true, sizeof(Vertex), (void*)offsetof(Vertex, normal));
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    
-    // glBindVertexArray(0);
     }
-            //TODO: (Req 2) Write this function
-            // remember to store the number of elements in "elementCount" since you will need it for drawing
-            // For the attribute locations, use the constants defined above: ATTRIB_LOC_POSITION, ATTRIB_LOC_COLOR, etc     
-    
         void draw() 
         {
-                // this function should render the mesh
+             // this function should render the mesh
+            //it is very important to bind the vertex array before drawing to record it
              glBindVertexArray(VAO);
-             glDrawElements(GL_TRIANGLES,elementCount, GL_UNSIGNED_SHORT, 0);
+             glDrawElements(GL_TRIANGLES,elementCount, GL_UNSIGNED_INT, 0);
             //TODO: (Req 2) Write this function
         }
 
--- source/common/texture/sampler.hpp
+++ source/common/texture/sampler.hpp
@@ -13,49 +13,67 @@ namespace our {
     public:
         // This constructor creates an OpenGL sampler and saves its object name in the member variable "name" 
         Sampler() {
+            
             //TODO: (Req 6) Complete this function
             // generate sampler and save it in name variable
+            // which are used to specify how textures are sampled in an OpenGL program
             // 1--->stands for one sampler object
+
             glGenSamplers(1, &name);
         };
+
         // This deconstructor deletes the underlying OpenGL sampler
         ~Sampler() { 
-            //TODO: (Req 6) Complete this function
-            // delete the sampler (name)
-            glDeleteSamplers(1, &name);
+        //TODO: (Req 6) Complete this function
+        // delete the sampler (name)
+        glDeleteSamplers(1, &name);
          }
 
         // This method binds this sampler to the given texture unit
         void bind(GLuint textureUnit) const {
-            //TODO: (Req 6) Complete this function
-            // bind the sampler to textureUnit-->0 or 1 or 2 ...
-                glBindSampler(textureUnit, name);
+        //TODO: (Req 6) Complete this function
+        //The texture unit number should be a value between 0 and GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS - 1.
+        // bind the sampler to textureUnit-->0 or 1 or 2 ...
+        glBindSampler(textureUnit, name);
         }
 
         // This static method ensures that no sampler is bound to the given texture unit
         static void unbind(GLuint textureUnit){
-            //TODO: (Req 6) Complete this function
+        //TODO: (Req 6) Complete this function
+
        //In this implementation, we simply call glBindSampler with the texture unit set to textureUnit and the sampler set to 0, which unbinds any sampler that was previously bound to that texture unit.
               glBindSampler(textureUnit, 0);
         }
 
         // This function sets a sampler paramter where the value is of type "GLint"
         // This can be used to set the filtering and wrapping parameters
+
+        // Filtering parameters control how texels (texture pixels) are sampled when a texture coordinate falls between texels, while wrapping parameters control how texture coordinates outside the range of [0, 1] are handled.
+        // For example, GL_TEXTURE_WRAP_S and GL_TEXTURE_WRAP_T are wrapping parameters that control how texture coordinates outside the range of [0, 1] are handled in the s and t directions, respectively. The possible values for these parameters are GL_REPEAT, GL_CLAMP_TO_EDGE, and GL_MIRRORED_REPEAT.
+        //Similarly, GL_TEXTURE_MIN_FILTER and GL_TEXTURE_MAG_FILTER are filtering parameters that control how texels are sampled when the texture is minified (shrunk) or magnified (enlarged), respectively. The possible values for these parameters are GL_NEAREST, GL_LINEAR, GL_NEAREST_MIPMAP_NEAREST, GL_LINEAR_MIPMAP_NEAREST, GL_NEAREST_MIPMAP_LINEAR, and GL_LINEAR_MIPMAP_LINEAR.
         void set(GLenum parameter, GLint value) const {
             //TODO: (Req 6) Complete this function
             // like : glSamplerParameteri(color_sampler, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glSamplerParameteri(name, parameter, value);
         }
 
+
         // This function sets a sampler paramter where the value is of type "GLfloat"
         // This can be used to set the "GL_TEXTURE_MAX_ANISOTROPY_EXT" parameter
+
+        // This parameter controls the amount of anisotropic filtering that is applied to the texture. Anisotropic filtering is a technique that improves the quality of textures when they are viewed at oblique angles by reducing the blurriness and improving the clarity of the texture.
+        // The GL_TEXTURE_MAX_ANISOTROPY_EXT parameter specifies the maximum degree of anisotropy to be used when filtering the texture.
         void set(GLenum parameter, GLfloat value) const {
             //TODO: (Req 6) Complete this function
         glSamplerParameterf(name, parameter, value);
         }
 
+
         // This function sets a sampler paramter where the value is of type "GLfloat[4]"
         // This can be used to set the "GL_TEXTURE_BORDER_COLOR" parameter
+
+        // This parameter specifies the color that is used for texture borders when the texture is accessed using texture coordinates that fall outside the range of [0,1].
+        // The value of this parameter is a four-component floating-point vector that specifies the red, green, blue, and alpha components of the border color.
         void set(GLenum parameter, glm::vec4 value) const {
             glSamplerParameterfv(name, parameter, &(value.r));
         }
