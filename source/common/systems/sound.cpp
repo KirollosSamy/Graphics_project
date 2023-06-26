3ec010552c1c9319864cff59f450803129e86faf
nadaaosman <84284180+nadaaosman@users.noreply.github.com>
2023-06-26 21:11:40 +0300

    finally , it is done ! ❤️

--- source/common/systems/sound.cpp
+++ source/common/systems/sound.cpp
@@ -14,23 +14,23 @@ namespace our
 
             switch (event)
             {
-            case Event::TERRIFIED:
-                SoundEngine->stopAllSounds();
-                SoundEngine->play2D("assets/audio/14. Spotted.mp3", false);
-                break;
-            case Event::NORMAL_MUSIC:
-                SoundEngine->stopAllSounds();
-                SoundEngine->play2D("assets/audio/29. Another Evil.mp3", true);
-                break;
-            case Event::BADROOM:
-                SoundEngine->stopAllSounds();
-                SoundEngine->play2D("assets/audio/baby_badroom.mp3", false);
-                break;
-                case Event::OPEN_DOOR:
-                std::cout << "reached here" << std::endl;
-                SoundEngine->stopAllSounds();
-                SoundEngine->play2D("assets/audio/openDoor.mp3", false);
-                break;
+                // case Event::TERRIFIED:
+                //     SoundEngine->stopAllSounds();
+                //     SoundEngine->play2D("assets/audio/14. Spotted.mp3", false);
+                //     break;
+                // case Event::NORMAL_MUSIC:
+                //     SoundEngine->stopAllSounds();
+                //     SoundEngine->play2D("assets/audio/29. Another Evil.mp3", true);
+                //     break;
+                // case Event::BADROOM:
+                //     SoundEngine->stopAllSounds();
+                //     SoundEngine->play2D("assets/audio/baby_badroom.mp3", false);
+                //     break;
+                //     case Event::OPEN_DOOR:
+                //     std::cout << "reached here" << std::endl;
+                //     SoundEngine->stopAllSounds();
+                //     SoundEngine->play2D("assets/audio/openDoor.mp3", false);
+                //     break;
             }
         }
     }
